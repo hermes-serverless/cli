90bcd2bc6f77f73543f3925c548a1b82d4674fa5
tiagonapoli <napoli.tiago96@gmail.com>
2019-06-14 17:20:48 -0300

    Add me command

--- src/commands/me.ts
+++ src/commands/me.ts
@@ -1,1 +1,16 @@
-export const meCommand = () => {}
+import { isLogged } from './../lib/authUtils'
+import { CommanderStatic } from 'commander'
+import chalk from 'chalk'
+
+export const meCommand = (program: CommanderStatic) => {
+  program.command('me').action(async cmd => {
+    const { state, username } = await isLogged()
+    if (state === 'Logged') {
+      console.log(`You're ` + chalk.green(`${username}`))
+    } else if (state === 'NotLogged') {
+      console.log(chalk.bold('You are not logged in!'))
+    } else if (state === 'TokenExpired') {
+      console.log(chalk.bold('Your token expired'))
+    }
+  })
+}
