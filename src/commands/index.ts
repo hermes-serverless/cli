2b2a8d5aa10dc11406e2843f51b28282470382ab
tiagonapoli <napoli.tiago96@gmail.com>
2019-06-28 18:34:49 -0300

    Add all new commands to cli

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,3 +1,5 @@
+import { listCommand } from './functions/list'
+import { deployCommand } from './functions/deploy'
 import { unregisterCommand } from './unregister'
 import { loginCommand } from './login'
 import { registerCommand } from './register'
@@ -6,6 +8,12 @@ import { logoutCommand } from './logout'
 import { CommanderStatic } from 'commander'
 import { initCommand } from './functions/init'
 import { functionBuildCommand } from './functions/build'
+import { configCommand } from './config'
+import { infoCommand } from './functions/info'
+import { deleteCommand } from './functions/delete'
+import { runCommand } from './run/run'
+import { functionRunListCommand } from './run/function-runs'
+import { runListCommand } from './run/runs'
 
 export interface CommandsObject {
   [command: string]: any
@@ -17,13 +25,21 @@ interface CommandProto {
 }
 
 const commandsToCreate: CommandProto[] = [
-  { name: 'register', creator: registerCommand },
-  { name: 'unregister', creator: unregisterCommand },
+  { name: 'build', creator: functionBuildCommand },
+  { name: 'config', creator: configCommand },
+  { name: 'delete', creator: deleteCommand },
+  { name: 'deploy', creator: deployCommand },
+  { name: 'function-runs', creator: functionRunListCommand },
+  { name: 'info', creator: infoCommand },
+  { name: 'init', creator: initCommand },
+  { name: 'list', creator: listCommand },
   { name: 'login', creator: loginCommand },
   { name: 'logout', creator: logoutCommand },
   { name: 'me', creator: meCommand },
-  { name: 'init', creator: initCommand },
-  { name: 'build', creator: functionBuildCommand },
+  { name: 'register', creator: registerCommand },
+  { name: 'run', creator: runCommand },
+  { name: 'runs', creator: runListCommand },
+  { name: 'unregister', creator: unregisterCommand },
 ]
 
 export const commandLoader = (program: CommanderStatic) => {
