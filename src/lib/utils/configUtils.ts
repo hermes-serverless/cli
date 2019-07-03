56d2e71fc2dac50b23277d9e74998934e8960259
tiagonapoli <napoli.tiago96@gmail.com>
2019-07-02 21:07:49 -0300

    Add configHermesURL function

--- src/lib/utils/configUtils.ts
+++ src/lib/utils/configUtils.ts
@@ -1,6 +1,7 @@
+import chalk from 'chalk'
 import inquirer from 'inquirer'
 import { Store } from '../../store'
-import { notEmpty } from './inquirerValidations'
+import { isURL, notEmpty } from './inquirerValidations'
 
 interface DockerhubUsernamePrompt {
   dockerhubUsername: string
@@ -17,4 +18,18 @@ export const configDockerhubUsername = async (val: string) => {
   ])
   Store.setDockerhubUsername(ans.dockerhubUsername)
   return ans.dockerhubUsername
+}
+
+export const configHermesURL = async (url: string) => {
+  const validate = (str: string) => {
+    const arr = [notEmpty("Hermes URL can't be empty")(str), isURL('Invalid URL')(str)]
+    for (let i = 0; i < arr.length; i += 1) {
+      if (arr[i] !== true) return arr[i]
+    }
+    return true
+  }
+
+  const msg = validate(url)
+  if (msg === true) return Store.setHermesURL(url)
+  console.log(chalk.bold.red(`${msg}`))
 }
