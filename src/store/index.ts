e843a232cbaf066c4af92ad3dbf372008416e693
tiagonapoli <napoli.tiago96@gmail.com>
2019-07-02 21:06:46 -0300

    Change url getter. Change to arrow functions

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,46 +3,45 @@ import { name as appName } from '../../package.json'
 
 export class Store {
   private static store = new ConfigStore(appName, {
-    hermesEndpointUrl: 'localhost',
-    hermesEndpointPort: 3000,
+    hermesEndpointUrl: '',
     token: '',
     dockerBin: '/usr/bin/docker',
     dockerhubUsername: '',
   })
 
-  public static getBaseUrl(protocol?: string) {
-    const url =
-      `${protocol || 'http'}://` +
-      this.store.get('hermesEndpointUrl') +
-      ':' +
-      this.store.get('hermesEndpointPort')
+  public static getBaseUrl = () => {
+    return Store.store.get('hermesEndpointUrl')
+  }
+
+  public static setHermesURL = (url: string) => {
+    Store.store.set('hermesEndpointUrl', url)
     return url
   }
 
-  public static addToken(token: string) {
-    this.store.set('token', token)
+  public static addToken = (token: string) => {
+    Store.store.set('token', token)
     return token
   }
 
-  public static getToken() {
-    return this.store.get('token')
+  public static getToken = () => {
+    return Store.store.get('token')
   }
 
-  public static getDockerPath() {
-    return this.store.get('dockerBin')
+  public static getDockerPath = () => {
+    return Store.store.get('dockerBin')
   }
 
-  public static setDockerPath(path: string) {
-    this.store.set('dockerBin', path)
+  public static setDockerPath = (path: string) => {
+    Store.store.set('dockerBin', path)
     return path
   }
 
-  public static getDockerhubUsername() {
-    return this.store.get('dockerhubUsername')
+  public static getDockerhubUsername = () => {
+    return Store.store.get('dockerhubUsername')
   }
 
-  public static setDockerhubUsername(username: string) {
-    this.store.set('dockerhubUsername', username)
+  public static setDockerhubUsername = (username: string) => {
+    Store.store.set('dockerhubUsername', username)
     return username
   }
 }
