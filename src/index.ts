d99d2347397b57e72c09e9b0b17b1355e577e83c
tiagonapoli <napoli.tiago96@gmail.com>
2019-07-07 12:38:28 -0300

    Print help when command is invalid

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import chalk from 'chalk'
 import program from 'commander'
 import projectPackage from '../package.json'
 import { commandLoader } from './commands'
@@ -6,4 +7,15 @@ import { commandLoader } from './commands'
 program.version(projectPackage.version)
 const commands = commandLoader(program)
 
+program.on('command:*', () => {
+  console.error(`${chalk.bold.red('Invalid command: ')} ${program.args.join(' ')}\n`)
+  program.help()
+  process.exit(1)
+})
+
 program.parse(process.argv)
+
+if (program.args.length === 0) {
+  program.help()
+  process.exit(1)
+}
